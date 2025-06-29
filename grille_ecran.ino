27881c8dea2d80719b015804850923b98e33b339
DanielsEsp <daniel.esp.iot@gmail.com>
2025-06-29 17:26:05 +0200

    Add files via upload

---
+++ grille_ecran.ino
@@ -0,0 +1,66 @@
+/*
+     drawBmp("/chambre.bmp", 0, 0);
+     tft.drawString("chambre", 100, 9, FONT4);          
+     tft.fillRect(80, 30, 155, 45, TFT_BLACK);
+     tft.drawString("-22", 80, 35, FONT6); tft.drawString("o", 155, 30, FONT4);
+     drawBmp("/humid_20.bmp", 155, 50);
+     tft.drawString("55", 180, 50, FONT4); tft.drawString("%", 210, 50, FONT4);
+     tft.drawRoundRect(0, 0, 80, 80, 5, TFT_GREEN);
+     
+     drawBmp("/salon.bmp", 240, 0);
+     tft.drawString("salon", 340, 9, FONT4);          
+     tft.fillRect(320, 30, 155, 45, TFT_BLACK);
+     tft.drawString(" 18", 320, 35, FONT6); tft.drawString("o", 395, 30, FONT4);
+     drawBmp("/humid_20.bmp", 395, 50);
+     tft.drawString("55", 420, 50, FONT4); tft.drawString("%", 450, 50, FONT4);
+     tft.drawRoundRect(240, 0, 80, 80, 5, TFT_ORANGE);
+
+     drawBmp("/bain.bmp", 0, 80);
+     tft.drawString("bain", 100, 89, FONT4);          
+     tft.fillRect(80, 110, 155, 45, TFT_BLACK);
+     tft.drawString(" 19", 80, 115, FONT6); tft.drawString("o", 155, 110, FONT4);
+     drawBmp("/humid_20.bmp", 155, 130);
+     tft.drawString("69", 180, 130, FONT4); tft.drawString("%", 210, 130, FONT4);
+     tft.drawRoundRect(0, 80, 80, 80, 5, TFT_GREEN);
+
+     drawBmp("/bureau.bmp", 240, 80);
+     tft.drawString("bureau", 340, 89, FONT4);          
+     tft.fillRect(320, 110, 155, 45, TFT_BLACK);
+     tft.drawString(" 23", 320, 115, FONT6); tft.drawString("o", 395, 110, FONT4);
+     drawBmp("/humid_20.bmp", 395, 130);
+     tft.drawString("55", 420, 130, FONT4); tft.drawString("%", 450, 130, FONT4);
+     tft.drawRoundRect(240, 80, 80, 80, 5, TFT_RED);
+
+     drawBmp("/garageOuvert.bmp", 0, 160);
+     tft.drawString("garage", 100, 169, FONT4);          
+     tft.fillRect(80, 190, 155, 45, TFT_BLACK);
+     tft.drawString(" 16", 80, 195, FONT6); tft.drawString("o", 155, 190, FONT4);
+     drawBmp("/humid_20.bmp", 155, 210);
+     tft.drawString("89", 180, 210, FONT4); tft.drawString("%", 210, 210, FONT4);
+     tft.drawRoundRect(0, 160, 80, 80, 5, TFT_GREEN);
+
+     drawBmp("/pergola.bmp", 240, 160);
+     tft.drawString("pergola", 340, 169, FONT4);          
+     tft.fillRect(320, 190, 155, 45, TFT_BLACK);
+     tft.drawString(" 23", 320, 195, FONT6); tft.drawString("o", 395, 190, FONT4);
+     drawBmp("/humid_20.bmp", 395, 210);
+     tft.drawString("55", 420, 210, FONT4); tft.drawString("%", 450, 210, FONT4);
+     tft.drawRoundRect(240, 160, 80, 80, 5, TFT_GREEN);
+
+     drawBmp("/piscine.bmp", 0, 240);
+     tft.drawString("piscine", 100, 249, FONT4);          
+     tft.fillRect(80, 270, 155, 45, TFT_BLACK);
+     tft.drawString(" 29", 80, 275, FONT6); tft.drawString("o", 155, 270, FONT4);
+     drawBmp("/humid_20.bmp", 155, 290);
+     tft.drawString("89", 180, 290, FONT4); tft.drawString("%", 210, 290, FONT4);
+     tft.drawRoundRect(0, 240, 80, 80, 5, TFT_GREEN);
+
+     drawBmp("/fosseOk.bmp", 240, 240);
+     tft.drawRoundRect(240, 240, 80, 80, 5, TFT_GREEN);
+
+     drawBmp("/celierOk.bmp", 320, 240);
+     tft.drawRoundRect(320, 240, 80, 80, 5, TFT_ORANGE);
+
+     drawBmp("/carportOuvert.bmp", 400, 240);
+     tft.drawRoundRect(400, 240, 80, 80, 5, TFT_RED);
+*/
